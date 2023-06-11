38b341bc26ebd3f236dd6c80690a4ab69c7fde11
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-06-11 22:16:43 +0700

    add json converter and update path operation

--- app/main.py
+++ app/main.py
@@ -18,21 +18,19 @@ async def root():
 
 @app.get("/students", status_code=status.HTTP_200_OK)
 async def get_all_students():
-    students = await db.students.find().to_list(1000)
-    return students
+    return [bson_to_dict(student) async for student in db.students.find()]
 
 
 @app.post("/students", status_code=status.HTTP_201_CREATED)
 async def add_student(student: StudentModel):
     student = jsonable_encoder(student)
-    result = await db.students.insert_one(student)
-    return student
+    await db.students.insert_one(student)
+    return bson_to_dict(student)
 
 
 @app.get("/students/{id}", status_code=status.HTTP_200_OK)
 async def get_student_by_id(id: int):
     student = await db.students.find_one({"_id": id})
-    if student is not None: 
-        return student
+    if student is not None:
+        return bson_to_dict(student)
     raise HTTPException(status_code=404, detail=f"Student {id} not found")
-
--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel, Field, EmailStr
-from bson import ObjectId
+from bson import json_util
+import json
 
 
 class StudentModel(BaseModel):
     student_id: int
     fullname: str
     email: EmailStr
-    gender: str = Field(..., regex='(?:m|M|male|Male|f|F|female|Female|FEMALE|MALE|Non-binary)$')
+    gender: str = Field(...,
+                        regex='(?:m|M|male|Male|f|F|female|Female|FEMALE|MALE|Non-binary)$')
     major: str
     year: int = Field(..., gt=0, lt=4)
     gpa: float = Field(..., ge=0, le=4)
@@ -14,22 +16,22 @@ class StudentModel(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "student_id": "1",
+                "student_id": 1,
                 "fullname": "Andrea Croke",
                 "email": "acrokeo4@123-reg.co.uk",
                 "gender": "Male",
                 "major": "Business Development",
                 "year": 1,
-                "gpa": 1.6,
+                "gpa": 1.6
             }
         }
 
 
 class UpdateStudentModel(BaseModel):
-    student_id: int | None
     fullname: str | None
     email: EmailStr | None
-    gender: str | None = Field(regex='(?:m|M|male|Male|f|F|female|Female|FEMALE|MALE|Non-binary)$')
+    gender: str | None = Field(
+        regex='(?:m|M|male|Male|f|F|female|Female|FEMALE|MALE|Non-binary)$')
     major: str | None
     year: int | None = Field(gt=0, lt=4)
     gpa: float | None = Field(ge=0, le=4)
@@ -37,13 +39,15 @@ class UpdateStudentModel(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "student_id": "1",
                 "fullname": "Andrea Croke",
                 "email": "acrokeo4@123-reg.co.uk",
                 "gender": "Male",
                 "major": "Business Development",
                 "year": 1,
-                "gpa": 1.6,
+                "gpa": 1.6
             }
         }
 
+
+def bson_to_dict(data):
+    return json.loads(json_util.dumps(data))
