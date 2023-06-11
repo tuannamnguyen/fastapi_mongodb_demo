1b004773cf797f75799c24e5e7195967f0427372
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-06-11 11:22:49 +0700

    update models and data

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@ from bson import ObjectId
 
 
 class StudentModel(BaseModel):
-    id: int
+    student_id: int
     fullname: str
     email: EmailStr
+    gender: str = Field(..., regex='(?:m|M|male|Male|f|F|female|Female|FEMALE|MALE|Non-binary)$')
     major: str
     year: int = Field(..., gt=0, lt=4)
     gpa: float = Field(..., ge=0, le=4)
@@ -13,9 +14,10 @@ class StudentModel(BaseModel):
     class Config:
         schema_extra = {
             "example": {
-                "student_id": 869,
+                "student_id": "1",
                 "fullname": "Andrea Croke",
                 "email": "acrokeo4@123-reg.co.uk",
+                "gender": "Male",
                 "major": "Business Development",
                 "year": 1,
                 "gpa": 1.6,
@@ -24,18 +26,21 @@ class StudentModel(BaseModel):
 
 
 class UpdateStudentModel(BaseModel):
+    student_id: int | None
     fullname: str | None
     email: EmailStr | None
+    gender: str | None = Field(regex='(?:m|M|male|Male|f|F|female|Female|FEMALE|MALE|Non-binary)$')
     major: str | None
-    year: int | None
-    gpa: float | None
+    year: int | None = Field(gt=0, lt=4)
+    gpa: float | None = Field(ge=0, le=4)
 
     class Config:
         schema_extra = {
             "example": {
-                "student_id": 869,
+                "student_id": "1",
                 "fullname": "Andrea Croke",
                 "email": "acrokeo4@123-reg.co.uk",
+                "gender": "Male",
                 "major": "Business Development",
                 "year": 1,
                 "gpa": 1.6,
