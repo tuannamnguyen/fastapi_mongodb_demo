7442267ae6859571f8784399327590d6a471cc81
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-06-13 17:04:33 +0700

    done login

--- app/users/user_router.py
+++ app/users/user_router.py
@@ -11,15 +11,26 @@ db = client.demoapp
 
 user_router = APIRouter()
 
+@user_router.get("", status_code=status.HTTP_200_OK)
+async def get_all_users():
+    return [bson_to_dict(user) async for user in db.users.find()]
+
 
 @user_router.post("/signup", status_code=status.HTTP_201_CREATED)
 async def user_signup(user: UserModel):
     user = jsonable_encoder(user)
+    # Insert hashed password into DB
     user.update({"password": get_password_hash(user["password"])})
     await db.users.insert_one(user)
     return bson_to_dict(user)
 
+
 @user_router.post("/login", status_code=status.HTTP_200_OK)
-async def user_login(user: UserLoginModel):
+async def user_login(user: UserLoginModel) -> bool:
     user = jsonable_encoder(user)
-   
+    user_in_db = await db.users.find_one({"email": user["email"]})
+    if not user_in_db:
+        return False
+    if not verify_password(user["password"], user_in_db["password"]):
+        return False
+    return True
