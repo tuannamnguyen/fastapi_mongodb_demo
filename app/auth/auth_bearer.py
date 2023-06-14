e37b781ca192ce40048462ef1896ba1292d064c7
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-06-14 09:01:45 +0700

    improve jwtbearer

--- app/auth/auth_bearer.py
+++ app/auth/auth_bearer.py
@@ -6,10 +6,10 @@ from .auth_handler import decodeJWT
 
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
-        super(JWTBearer, self).__init__(auto_error=auto_error)
+        super().__init__(auto_error=auto_error)
 
     async def __call__(self, request: Request):
-        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        credentials: HTTPAuthorizationCredentials = await super().__call__(request)
         if credentials:
             if not credentials.scheme == "Bearer":
                 raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
