b512932a7305f396d12b2e10cc88cd906d3949bf
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-06-14 11:18:07 +0700

    done oauth2

--- app/auth/auth_bearer.py
+++ app/auth/auth_bearer.py
@@ -1,38 +1,13 @@
-from fastapi import Request, HTTPException, Depends, status
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials, OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
 from typing import Annotated
 from jwt import PyJWTError
 
-from .auth_handler import *
+from app.auth.auth_handler import *
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
-class JWTBearer(HTTPBearer):
-    def __init__(self, auto_error: bool = True):
-        super().__init__(auto_error=auto_error)
 
-    async def __call__(self, request: Request):
-        credentials: HTTPAuthorizationCredentials = await super().__call__(request)
-        if credentials:
-            if not credentials.scheme == "Bearer":
-                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
-            if not self.verify_jwt(credentials.credentials):
-                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
-            return credentials.credentials
-        else:
-            raise HTTPException(status_code=403, detail="Invalid authorization code.")
-
-    def verify_jwt(self, jwtoken: str) -> bool:
-        isTokenValid: bool = False
-
-        try:
-            payload = decodeJWT(jwtoken)
-        except:
-            payload = None
-        if payload:
-            isTokenValid = True
-        return isTokenValid
-    
 async def jwt_bearer(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -41,9 +16,8 @@ async def jwt_bearer(token: Annotated[str, Depends(oauth2_scheme)]):
     )
     try:
         payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        email: str = payload.get("email")
-        if email is None:
+        username: str = payload.get("username")
+        if username is None:
             raise credentials_exception
     except PyJWTError:
         raise credentials_exception
-        
