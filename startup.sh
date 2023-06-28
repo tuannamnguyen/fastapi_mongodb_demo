374371e2c7a3534f3c4f079d934e3e758262ce0d
tuannamnguyen <tuannamnguyen290602@gmail.com>
2023-06-28 16:14:47 +0700

    update startup command

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 docker build -t myimage .
-docker run --rm --env-file ./.env -p 8000:8000 --name mycontainer myimage
+docker run -d --rm -p 6379:6379 --name myredis --network fastapi-demo redis
+docker run -d --rm --env-file ./.env -p 8000:8000 --name mycontainer --network fastapi-demo myimage 
